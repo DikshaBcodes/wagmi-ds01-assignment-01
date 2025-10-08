7b3551c98a67ee36b42c14f2590180a5f1d3ab5a
Diksha Bothra <dbothraworks@gmail.com>
2025-10-08 18:18:11 +0530

    Stack done

--- src/main/java/com/wagmi/finance/history/OperationStack.java
+++ src/main/java/com/wagmi/finance/history/OperationStack.java
@@ -6,30 +6,58 @@ package main.java.com.wagmi.finance.history;
  - Note dynamic growth is allowed only when initial capacity >= 10.
  - Validate null pushes (should throw).
  - Run `OperationStackTest` after changes.
-*/
-
+ */
 public class OperationStack {
+
     private final String[] data;
     private int top = -1;
 
     public OperationStack(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("There are elements in the stack");
+        }
         this.data = new String[capacity];
     }
 
     public void push(String operation) {
-        // stub
-        throw new UnsupportedOperationException("Not implemented");
+        if (operation == null) {
+            throw new IllegalArgumentException("Operation cannot be null");
+        }
+         if (top==-1){
+            top=0;
+            data[top]=operation;
+        }
+        else if(top==data.length-1)
+        {throw new UnsupportedOperationException("Stack overflow");}
+        else{top++;
+        data[top]=operation;}
+        
+
     }
 
     public String pop() {
-        // stub
-        throw new UnsupportedOperationException("Not implemented");
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack underflow — nothing to pop!");
+        }
+        else{
+        String value = data[top];
+
+         top--;
+
+        return value;
+        }
+
     }
 
     public String peek() {
-        // stub
-        throw new UnsupportedOperationException("Not implemented");
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty —there is nothing to peek!");
+        }
+        return data[top];
     }
+        
+        
+    
 
     public boolean isEmpty() {
         return top < 0;
